33efb7b7ca803f4e41428ab2aa563ba198241b38
Olga Lapuhina <olgasserg@mail.ru>
2022-08-09 00:43:44 +0500

    Added a method mediumCropped

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         salesManager.sales = new int[]{1000, 2000, 3000, 1500, 2500};
 
         System.out.println("Максимальная сумма продаж: " + salesManager.max());
+        System.out.println("Расчет обрезанного среднего равен: " + salesManager.mediumCropped());
     }
 }
