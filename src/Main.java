4d22857ba2fef8a839fdd2ddfc55ec890c253dc7
Olga Lapuhina <olgasserg@mail.ru>
2022-08-08 23:12:55 +0500

    Replaced the type of sales from int to long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[5]);
-        salesManager.sales = new int[]{1000, 2000, 3000, 1500, 2500};
+        SalesManager salesManager = new SalesManager(new long[5]);
+        salesManager.sales = new long[]{1000, 2000, 3000, 1500, 2500};
 
-        System.out.println("Максимальная сумма продажи среди сотрудников: " + salesManager.max());
+        System.out.println("РњР°РєСЃРёРјР°Р»СЊРЅР°СЏ СЃСѓРјРјР° РїСЂРѕРґР°Р¶Рё СЃСЂРµРґРё СЃРѕС‚СЂСѓРґРЅРёРєРѕРІ: " + salesManager.max());
     }
 }
